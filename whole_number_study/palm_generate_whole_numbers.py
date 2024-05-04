15a1dff1a42fc1f2e0256f09767619925e8881c2
kevinez2 <kevinez2@illinois.edu>
2024-05-03 22:20:58 -0500

    Wrote Whole number generation code.

--- whole_number_study/palm_generate_whole_numbers.py
+++ whole_number_study/palm_generate_whole_numbers.py
@@ -1,9 +1,16 @@
+import ast
 import pprint
 import google.generativeai as palm
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 import random
+from google.api_core import retry
+
+@retry.Retry()
+def generate_text(*args, **kwargs):
+    response = palm.generate_text(*args, **kwargs).candidates
+    return [output['output'] for output in response]
 
 def generate_numbers(api_key, prompt, output_file):
     np.random.seed(0)
@@ -11,41 +18,47 @@ def generate_numbers(api_key, prompt, output_file):
     models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
     model = models[0].name
     print(model)
-
-    random_numbers = []
-    while len(random_numbers) < 10000:
-        #"""Generate 10,000 uniformly distributed random numbers between 0 and 1. Previous numbers should have no influence on future numbers: """
-        #add 20 random numbers between 0 and 1
-        for i in range(20):
+    temp = 1.0
+    random.seed(1234)
+    for i in range(20):
             prompt += str(random.randint(0, 10000)) + " "
+    for temp in [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]:
+        random_numbers = []
+        while len(random_numbers) < 10000:
+            responses = generate_text(
+                model=model,
+                prompt=prompt,
+                temperature=temp,
+                # The maximum length of the response
+                max_output_tokens=1024,
+                candidate_count=8
+            )
+            # print(completion.result)
+            for response in responses:
+                try:
+                    random_numbers += [int(num) for num in response.split()[:-1]]
+                except:
+                    continue
+                print(len(random_numbers))
 
-        # print(prompt)
-        completion = palm.generate_text(
-            model=model,
-            prompt=prompt,
-            temperature=0,
-            # The maximum length of the response
-            max_output_tokens=800,
-        )
-
-        # print(completion.result)
-        try:
-            random_numbers += [int(num) for num in completion.result.split()[:-1]]
-        except:
-            continue
-        print(len(random_numbers))
-
-    random_numbers = random_numbers[:10000]
-    print(random_numbers)
-    # save the random numbers to a file
-    with open(output_file, 'w') as f:
-        for num in random_numbers:
-            f.write(str(num) + '\n')
+        random_numbers = random_numbers[:10000]
+        # save the random numbers to a file
+        with open(output_file + "_" + str(temp).replace(".", "_") + ".txt", 'w') as f:
+            for num in random_numbers:
+                f.write(str(num) + '\n')
 
 def plot_distribution(random_numbers):
     # plot the distribution of the random numbers
     plt.hist(random_numbers, bins=100)
     plt.show()
+
+def test_random_seed():
+    int_list = []
+    for _ in range(5):
+        random.seed(1234)
+        for _ in range(20):
+            int_list.append(str(random.randint(0, 10000)))
+    print(int_list)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -53,4 +66,5 @@ if __name__ == "__main__":
     parser.add_argument('--prompt', type=str, help='Your prompt')
     parser.add_argument('--output_file', type=str, help='output file name')
     args = parser.parse_args()
+    #test_random_seed()
     generate_numbers(args.api_key, args.prompt, args.output_file)
